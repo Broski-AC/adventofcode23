8af635a5d601adb0f8556f85b85189cda959bd07
Allison Broski <broski.ac@gmail.com>
2023-12-10 22:06:51 -0600

    feat: finish part one

--- day_four/one.py
+++ day_four/one.py
@@ -7,32 +7,13 @@ def scrub(list):
 def main():
     points = 0
 
-    # with open("small_input.txt") as file:
-    #     for line in file:   
-    #         card = line.split(": ")[1]
-    #         winning_values, actual_values = card.split(" | ")
-
-    #         win_vals = scrub(winning_values.split(" "))
-    #         act_vals = scrub(actual_values.split(" "))
-            
-    #         num_matches = matching(win_vals, act_vals)
-
-    #         if num_matches == 0:
-    #             next
-    #         else:
-    #             current_points = 1 * pow(2, num_matches - 1)
-    #             points += current_points
-    #             print(current_points)
-
-    #     print(points)
-
-    with open("small_input.txt") as file:
+    with open("input.txt") as file:
         for line in file:
             card = line.split(": ")[1]
             winning_values, actual_values = card.split(" | ")
 
-            win_vals = scrub(winning_values.split(" "))
-            act_vals = scrub(actual_values.split(" "))
+            win_vals = scrub(winning_values.replace("\n", "").split(" "))
+            act_vals = scrub(actual_values.replace("\n", "").split(" "))
             
             num_matches = matching(win_vals, act_vals)
 
@@ -41,7 +22,8 @@ def main():
             else:
                 current_points = 1 * pow(2, num_matches - 1)
                 points += current_points
-            print(points)
+            
+        print(points)
 
 
 if __name__ == "__main__":
