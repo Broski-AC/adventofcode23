2e6017cad9fae648df769e8800d0fbb97145f2ea
Allison Broski <broski.ac@gmail.com>
2023-12-10 21:59:10 -0600

    feat: trying some code

---
+++ day_four/one.py
@@ -0,0 +1,48 @@
+def matching(win, act):
+    return sum(val in act for val in win)
+
+def scrub(list):
+    return [item for item in list if item.isdigit()]
+
+def main():
+    points = 0
+
+    # with open("small_input.txt") as file:
+    #     for line in file:   
+    #         card = line.split(": ")[1]
+    #         winning_values, actual_values = card.split(" | ")
+
+    #         win_vals = scrub(winning_values.split(" "))
+    #         act_vals = scrub(actual_values.split(" "))
+            
+    #         num_matches = matching(win_vals, act_vals)
+
+    #         if num_matches == 0:
+    #             next
+    #         else:
+    #             current_points = 1 * pow(2, num_matches - 1)
+    #             points += current_points
+    #             print(current_points)
+
+    #     print(points)
+
+    with open("small_input.txt") as file:
+        for line in file:
+            card = line.split(": ")[1]
+            winning_values, actual_values = card.split(" | ")
+
+            win_vals = scrub(winning_values.split(" "))
+            act_vals = scrub(actual_values.split(" "))
+            
+            num_matches = matching(win_vals, act_vals)
+
+            if num_matches == 0:
+                next
+            else:
+                current_points = 1 * pow(2, num_matches - 1)
+                points += current_points
+            print(points)
+
+
+if __name__ == "__main__":
+    main()
